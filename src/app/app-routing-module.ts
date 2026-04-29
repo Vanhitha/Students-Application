ccddd0084dd02e6fd280fd19d76dbd1befe97469
Vanhitha <vanhitha.karnati@desidea.com>
2026-04-29 22:42:13 +0530

    for testing

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -13,14 +13,6 @@ import { ProfileSettings } from './profile-settings/profile-settings';
 import { Settings } from './settings/settings';
 import { Notifications } from './notifications/notifications';
 import { Securitysetings } from './securitysetings/securitysetings';
-import { AddStudent } from './add-new/add-student/add-student';
-import { AddTeacher } from './add-new/add-teacher/add-teacher';
-import { StudentDashboard } from './dashboard/student-dashboard/student-dashboard';
-import { AddStaf } from './add-new/add-staf/add-staf';
-import { TeacherDashboard } from './dashboard/teacher-dashboard/teacher-dashboard';
-import { ParentDashboard } from './parent-dashboard/parent-dashboard';
-import { DashboardModule } from './dashboard/dashboard-module';
-
 const routes: Routes = [
   {path:"",component:Login},
   {path:"register",component:Register},
